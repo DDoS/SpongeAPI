9de4558383106700af84417b0e1a355042fd5d3a
gratimax <max@ovsankin.com>
2015-02-28 22:16:40 -0800

    Add TileTypes

---
+++ src/main/java/org/spongepowered/api/block/tile/TileTypes.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.api.block.tile;
+
+/**
+ * An enumeration of all {@link TileType}s in vanilla Minecraft.
+ */
+public class TileTypes {
+    private TileTypes() {}
+
+    public static final TileType BANNER = null;
+    public static final TileType BEACON = null;
+    public static final TileType BREWING_STAND = null;
+    public static final TileType CHEST = null;
+    public static final TileType COMMAND_BLOCK = null;
+    public static final TileType COMPARATOR = null;
+    public static final TileType DAYLIGHT_DETECTOR = null;
+    public static final TileType DISPENSER = null;
+    public static final TileType DROPPER = null;
+    public static final TileType ENCHANTMENT_TABLE = null;
+    public static final TileType ENDER_CHEST = null;
+    public static final TileType FLOWER_POT = null;
+    public static final TileType FURNACE = null;
+    public static final TileType HOPPER = null;
+    public static final TileType JUKEBOX = null;
+    public static final TileType MOB_SPAWNER = null;
+    public static final TileType NOTE = null;
+    public static final TileType PISTON = null;
+    public static final TileType SIGN = null;
+    public static final TileType SKULL = null;
+
+}
